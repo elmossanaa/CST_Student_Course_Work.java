763643781bd54cd4266d00d531edf384dfb5335c
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2022-03-22 22:56:00 -0400

    Create  Utilize_a_switch_Statement.java

---
+++ Utilize_a_switch_Statement.java
@@ -0,0 +1,19 @@
+public class switch_statement {
+
+    
+    public static void main(String[] args) {
+    //if a equals 10 modulus a by 2 and print result to console
+    int a =10;
+    switch(a){
+        case 1:
+            System.out.println(a+5);
+            break;
+        case 2:
+            System.out.println(a+15);
+            break;
+        case 10:
+            System.out.println(a%2);
+            break;
+    }
+    }
+}
