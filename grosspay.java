08ddf55c3cd18bee83529b3f759f8810f46dadcb
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-04 19:15:57 -0400

    Create grosspay.java

---
+++ grosspay.java
@@ -0,0 +1,9 @@
+public class Algebracalculation {
+
+    public static void main(String[] args) {
+        int hour = 26;
+        double grosspay, payrate = 11;
+        grosspay = hour * payrate;
+        System.out.println("your grosspay is " + grosspay);
+    }
+}
