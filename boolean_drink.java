fae210ddf0950031d7924e749742319e988d4df1
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-04 21:24:42 -0400

    Create boolean_drink.java

---
+++ boolean_drink.java
@@ -0,0 +1,10 @@
+public class drinks {
+    public static void main(String[] args){
+        boolean water = true;
+        boolean soda = false;
+        System.out.println("water is healthy : statement is " + water );
+        System.out.println("soda is healthy : statement is " + soda);
+     
+    }
+    
+}
