e91e0abea59aed8f63d21607711f754b9a40b7ab
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-05 12:07:45 -0500

    Create item_saleprice.java

---
+++ item_saleprice.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+        double item = 59.0;
+        double discount = item * 0.20;
+        double saleprice;
+        saleprice = item - discount ;
+        System.out.println("discount price is " + discount);
+        System.out.println("sale price of item is " + saleprice);
+          
+        
+    }
+    
+}
