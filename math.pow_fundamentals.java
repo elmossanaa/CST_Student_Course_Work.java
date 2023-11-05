3746a702b04ce0490e3398488b619abc02f91115
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-05 13:28:58 -0500

    math.pow_fundamentals.java

---
+++ math.pow_fundamentals.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+       double a = 2.0;
+       double b = 4.0;
+       double result = Math.pow(a,b); //result = 4^2
+        System.out.println(result); 
+        double x = 3.0;
+        double result_n2= x*Math.pow(6.0,3.0);
+        System.out.println(result_n2);
+       
+       
+    }
+    
+}
