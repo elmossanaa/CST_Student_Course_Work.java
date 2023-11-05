67cb922b206fce666c6c8955def99f35af822be2
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-05 13:36:10 -0500

    math.sqrt.java

---
+++ math.sqrt.java
@@ -0,0 +1,9 @@
+  public static void main(String[] args) {
+        
+        System.out.println(Math.sqrt(25));
+        int m = 36;
+        int n =  (int) Math.sqrt(m);
+        System.out.println(n);
+    }
+    
+}
