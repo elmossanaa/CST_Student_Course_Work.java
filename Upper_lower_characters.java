3712f7922582e62f633baf8be0ade5249f4dc9be
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-06 18:05:01 -0500

    Upper_lower_characters.java

---
+++ Upper_lower_characters.java
@@ -0,0 +1,13 @@
+public class String_upper_or_lowercase_words {
+
+    public static void main(String[] args) {
+        String thoughts = "Java is fun!";
+        String upper = thoughts.toUpperCase();
+        System.out.println(thoughts);
+        System.out.println("\n"+ upper);
+        String message = "JAVA IS COOL!";
+        String lower = message.toLowerCase();
+        System.out.println(message);
+        System.out.println(lower);
+    }
+}
