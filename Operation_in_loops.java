b46a58de6f57a9bd0c7657a3ef0ba4a4e3cda29f
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-26 21:29:31 -0500

    Operation_in_loops.java

---
+++ Operation_in_loops.java
@@ -0,0 +1,28 @@
+public class Operation_in_loops {
+
+
+    public static void main(String[] args) {
+       int num = 20;
+       int n2 = 33;
+        System.out.println("Number is " + num);
+        num++;
+        System.out.println("The number incremented is: " + num);
+        --num;
+        System.out.println("number is now back to: " + num);
+        System.out.println("Next number is: " + n2);
+        n2--;
+        System.out.println("This number is decremented to: " + n2);
+        ++n2;
+        System.out.println("number back to: " + n2);
+        
+        int u = 1;
+        
+        
+        while(u<5){
+            
+            System.out.println("Coding is life!");
+            u++;
+              }
+        
+        }
+    }
