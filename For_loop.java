b8e5a605da87a25492f1ffe1b2280be53b2e902d
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-11-28 01:19:34 -0500

    For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,17 @@
+public class For_loop {
+
+    public static void main(String[] args) {
+        
+        int num;
+        System.out.println("Number          Number Squared");
+        System.out.println("------------------------------------");
+        for(num=1; num<=10;num++){
+            System.out.println(num + "\t\t\t" + num*num);
+        }
+        
+        
+        
+        
+        
+    }
+}
