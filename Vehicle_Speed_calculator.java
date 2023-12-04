20ee1feb9b50ab201706250e8c21e375b3c8bbda
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-12-04 17:27:19 -0500

    Create  Vehicle_Speed_calculator.java
    // CST1201 coursework Assignment

---
+++ Vehicle_Speed_calculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Wordproblems {
+    /**The distance a vehicle travels can be calculated as follows:
+Distance = Speed * Time
+* 
+For example, if a train travels 40 miles-per-hour for three hours, the distance traveled is 
+120 miles. Write a program that asks for the speed of a vehicle (in miles-per-hour) and the 
+number of hours it has traveled. It should use a loop to display the distance a vehicle has 
+traveled for each hour of a time period specified by the user. For example, if a vehicle is 
+traveling at 40 mph for a three-hour time period, it should display a report similar to the 
+one that follows:
+* 
+* 
+* Hour --------Distance Traveled -----------------
+1                        40
+2                        80
+3                       120
+* 
+* 
+Input Validation: Do not accept a negative number for speed and do not accept any value 
+less than 1 for time traveled.**/
+
+    public static void main(String[] args) {
+        
+        int distance;
+        int speed;
+        int hour;
+        
+       Scanner driver = new Scanner(System.in);
+       
+        System.out.println("Please enter your vehicle speed (in mph) :) : ");
+        speed = driver.nextInt();
+        
+        System.out.println("Please enter the number of hours your vehicle has travelled :) :");
+        hour = driver.nextInt();
+        
+        distance = speed*hour;
+        
+        System.out.println("Hour\tDistance Travled(mph)");
+        System.out.println("-------------------------------------------------------------");
+        
+        for(int car =1; car<=hour; car++){
+            
+            System.out.println(car +"\t" + (car*speed) );
+        }
+      
+      
+      
+    }   
+        }
