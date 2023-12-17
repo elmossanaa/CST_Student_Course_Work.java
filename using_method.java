574037d1b109856ab960aa727dac5f41c1c0956d
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-12-17 16:14:38 -0500

    Using_method.java

---
+++ using_method.java
@@ -0,0 +1,12 @@
+public class Using_method {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! From the main method.");
+        SecondMessage();
+        System.out.println("Back in the main method");
+    }
+    public static void SecondMessage(){
+        System.out.println("Hello from the second display message method");
+    
+}
+}
