1dc50d5b7d329deab943b7275778da2b26e65db9
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2023-12-20 12:33:15 -0500

    Delete String_name.java

--- String_name.java
+++
@@ -1,9 +0,0 @@
- public static void main(String[] args) {
-        String name = "suzy";
-        System.out.println("good afternoon " + name +"!");
-        String n2 = "Victoria";
-        int name_size = n2.length();
-        System.out.println(n2 + " has " + name_size + " letters in her name.");
-  
-        
-    }
